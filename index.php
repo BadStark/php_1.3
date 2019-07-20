31a748e8927982f9eb500183ca4e2c02350478fa
BadStark <bad-stark@yandex.ru>
2019-07-20 23:29:00 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP-1</title>
+</head>
+
+<?php
+
+var_dump(include __DIR__ . '/1.php');
+var_dump(include __DIR__ . '/2.php');  //
+var_dump(include __DIR__ . '/3.php');
+
+?>
+</body>
+</html>
